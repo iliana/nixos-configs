c1334193f03151e43886dc260b393a0339f0f865
iliana etaoin <iliana@buttslol.net>
2023-03-12 06:39:59 +0000

    persist host SSH keys

--- profiles/base.nix
+++ profiles/base.nix
@@ -35,6 +35,10 @@
     ];
     iliana.persist.files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
 
     time.timeZone = "Etc/UTC";
