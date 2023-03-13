e863d886ca6ce57731cd9c71fb8ceba22ffdd2a9
iliana etaoin <iliana@buttslol.net>
2023-03-13 06:52:00 +0000

    print deleted stale dotfiles links

--- profiles/dotfiles.sh
+++ profiles/dotfiles.sh
@@ -2,4 +2,4 @@
 ln -sfn "$1" ~/.dotfiles
 cp -rsf --no-preserve=mode,ownership ~/.dotfiles/. ~
 # find broken links to ~/.dotfiles and delete them
-find ~ -lname ~/.dotfiles/'*' -xtype l -delete
+find ~ -lname ~/.dotfiles/'*' -xtype l -delete -print
