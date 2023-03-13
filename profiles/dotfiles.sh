e651f5ef7d0ec900b2d4e6418238bdaefd6b046c
iliana etaoin <iliana@buttslol.net>
2023-03-13 06:50:30 +0000

    more dotfiles permission fix

--- profiles/dotfiles.sh
+++ profiles/dotfiles.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=bash
 ln -sfn "$1" ~/.dotfiles
-cp -rsf ~/.dotfiles/. ~
+cp -rsf --no-preserve=mode,ownership ~/.dotfiles/. ~
 # find broken links to ~/.dotfiles and delete them
 find ~ -lname ~/.dotfiles/'*' -xtype l -delete
