5332b797e1a1e9d2c86c543062ac977ab23ecf56
iliana etaoin <iliana@buttslol.net>
2023-04-06 07:33:22 +0000

    blah

--- flake.nix
+++ flake.nix
@@ -7,17 +7,26 @@
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
     impermanence.url = "github:nix-community/impermanence";
 
     dotfiles.url = "github:iliana/dotfiles?dir=.config/dotfiles&submodule=1";
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, crane, impermanence, ... }@inputs:
+  outputs =
+    { nixpkgs
+    , nixpkgs-unstable
+    , crane
+    , flake-utils
+    , impermanence
+    , ...
+    }@inputs:
     let
+      inherit (flake-utils.lib) system;
       lib = nixpkgs.lib;
-      systems = [ "x86_64-linux" ];
+      eachSystem = lib.genAttrs [ system.x86_64-linux ];
 
-      packages = nixpkgs.lib.genAttrs systems (system:
+      packages = eachSystem (system:
         let
           pkgs = import nixpkgs { inherit system; };
           craneLib = crane.lib.${system};
@@ -25,27 +34,26 @@
         {
           emojos-dot-in = pkgs.callPackage ./packages/emojos-dot-in.nix { inherit craneLib; };
         });
+      pkgs-unstable = eachSystem (system: import nixpkgs-unstable { inherit system; });
 
-      hosts = system:
-        let
-          pkgs-unstable = import nixpkgs-unstable { inherit system; };
-        in
-        hosts: lib.attrsets.genAttrs hosts (host: lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/${host}.nix
-            impermanence.nixosModules.impermanence
-          ];
-          specialArgs = {
-            inherit inputs pkgs-unstable;
-            pkgs-iliana = packages.${system};
-          };
-        });
+      hosts = builtins.mapAttrs (host: system: lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/${host}.nix
+          impermanence.nixosModules.impermanence
+        ];
+        specialArgs = {
+          inherit inputs;
+          pkgs-iliana = packages.${system};
+          pkgs-unstable = pkgs-unstable.${system};
+        };
+      });
     in
     {
       inherit packages;
-      nixosConfigurations = hosts "x86_64-linux" [
-        "hydrangea"
-      ];
+
+      nixosConfigurations = hosts {
+        hydrangea = system.x86_64-linux;
+      };
     };
 }
