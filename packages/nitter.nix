6363e19214bc93fce4824f3814c458aaff45e0aa
iliana etaoin <iliana@buttslol.net>
2023-04-14 19:33:45 +0000

    Revert "fix version on nitter about page"
    This reverts commit fd40d0a84aae8e014cc6442821067b4444a8bf77.

--- packages/nitter.nix
+++ packages/nitter.nix
@@ -1,12 +1,9 @@
-{ nitter, gitMinimal, fetchFromGitHub, ... }: nitter.overrideAttrs (old: {
+{ nitter, fetchFromGitHub, ... }: nitter.overrideAttrs (old: {
   version = "unstable-2023-03-28";
   src = fetchFromGitHub {
-    leaveDotGit = true;
     owner = "zedeus";
     repo = "nitter";
-    rev = "95893eedaa2fb0ca0a0a15257d81b720f7f3eb67";
-    hash = "sha256-3lQgKPnEagpJi1YT69ANZUpwhjY3HhdhKpkbsQo2Xn0=";
+    rev = "23f4c6114c2b790405a9cefef1a7979655de44d4";
+    hash = "sha256-Tv10M/t1b6KwS702duVZK58sVfLup4aOemT/I2k8rp4=";
   };
-  nativeBuildInputs = old.nativeBuildInputs ++ [ gitMinimal ];
-  patches = [ ];
 })
