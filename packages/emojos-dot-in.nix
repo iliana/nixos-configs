9a7022f97e3a9e31a86f63d70984a27e03b3f2a8
iliana etaoin <iliana@buttslol.net>
2023-04-29 17:37:34 +0000

    update emojos

--- packages/emojos-dot-in.nix
+++ packages/emojos-dot-in.nix
@@ -5,8 +5,8 @@
   src = fetchFromGitHub {
     owner = "iliana";
     repo = "emojos.in";
-    rev = "92c717d9fd96870d18addd8a3980d6cb72c3c7f4";
-    sha256 = "sha256-VcV7kXWzj94GE/6AepVkfpcpAIH7kRTotZOiQu13YBE=";
+    rev = "e0152d4bb55ea59e9d58cccfcdcf51a7b9ce4f86";
+    sha256 = "sha256-l+17dLSQM9lI1tJqjP5j957hY9oIDyeDBIYk2pbikFM=";
   };
 
   buildInputs = [ pkg-config openssl ];
