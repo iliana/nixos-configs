399edecb3c2601d91ab2aaf8513654bc6bd9a15b
iliana etaoin <iliana@buttslol.net>
2023-05-02 05:25:35 +0000

    add /etc/iliana-rev

--- lib/registry.nix
+++ lib/registry.nix
@@ -20,4 +20,6 @@ in
   nix.registry.nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
 
   nix.settings.flake-registry = pkgs.writeText "flake-registry.json" (builtins.toJSON { flakes = [ ]; version = 2; });
+
+  environment.etc."iliana-rev".text = if (inputs.self ? rev) then "${inputs.self.rev}\n" else "";
 }
