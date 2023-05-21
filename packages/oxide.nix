364079aea7dd975cb83a97da0d813fdf318e7caf
iliana etaoin <iliana@buttslol.net>
2023-05-21 22:16:22 +0000

    avoid 2-step build for oxide CLI
    ... as the target.tar.zst is far too large to be useful

--- packages/oxide.nix
+++ packages/oxide.nix
@@ -3,19 +3,14 @@
   openssl,
   oxide-cli,
   pkg-config,
-}: let
-  commonArgs = {
-    pname = "oxide";
-    version = "0.1.0";
-    src = oxide-cli;
-    cargoExtraArgs = "--package oxide";
-    doCheck = false;
-    buildInputs = [pkg-config openssl];
-  };
-  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-in
-  craneLib.buildPackage (commonArgs
-    // {
-      inherit cargoArtifacts;
-      patches = [./oxide-git-version.patch];
-    })
+}:
+craneLib.buildPackage {
+  pname = "oxide";
+  version = "0.1.0";
+  src = oxide-cli;
+  cargoArtifacts = null;
+  cargoExtraArgs = "--package oxide";
+  doCheck = false;
+  buildInputs = [pkg-config openssl];
+  patches = [./oxide-git-version.patch];
+}
