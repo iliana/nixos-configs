2fc6ab38e05846bae1d745b1daeb7a79353d6103
iliana etaoin <iliana@buttslol.net>
2023-05-24 05:50:02 +0000

    lernie: let build user log in and build stuff

--- hosts/lernie.nix
+++ hosts/lernie.nix
@@ -5,7 +5,10 @@
 
   users.users.build.group = "build";
   users.users.build.isSystemUser = true;
+  users.users.build.useDefaultShell = true;
   users.groups.build = {};
+
+  nix.settings.trusted-users = ["root" "build"];
 
   system.stateVersion = "22.11";
 }
