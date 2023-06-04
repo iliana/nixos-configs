20debff42d2ed4ef89a4dbbc825169a9d3e50f66
iliana etaoin <iliana@buttslol.net>
2023-06-04 23:37:17 +0000

    ugh hetzner

--- hosts/alecto.nix
+++ hosts/alecto.nix
@@ -7,5 +7,17 @@
 
   iliana.hardware.biosBootDevice = "/dev/sda";
   iliana.hardware.useSerialConsole = false;
+  networking.usePredictableInterfaceNames = false;
+  networking.interfaces.eth0.ipv6.addresses = [
+    {
+      address = "2a01:4ff:1f0:c2de::1";
+      prefixLength = 64;
+    }
+  ];
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "eth0";
+  };
+
   system.stateVersion = "23.05";
 }
