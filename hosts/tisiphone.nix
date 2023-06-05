3a9dd79308d0655949106ef63916b3abd6546208
iliana etaoin <iliana@buttslol.net>
2023-06-05 08:24:39 +0000

    oh right hetzner ipv6

--- hosts/tisiphone.nix
+++ hosts/tisiphone.nix
@@ -4,6 +4,17 @@
   ];
 
   iliana.hardware.useSerialConsole = false;
+  networking.usePredictableInterfaceNames = false;
+  networking.interfaces.eth0.ipv6.addresses = [
+    {
+      address = "2a01:4f8:c17:f980::1";
+      prefixLength = 64;
+    }
+  ];
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "eth0";
+  };
 
   iliana.buildHost = true;
   iliana.pdns.enable = true;
