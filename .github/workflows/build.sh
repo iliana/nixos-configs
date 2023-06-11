341575f5686ef3ab58992ac3b1daf252559d62c9
iliana etaoin <iliana@buttslol.net>
2023-06-11 23:46:04 +0000

    move inline run script into separate file

---
+++ .github/workflows/build.sh
@@ -0,0 +1,16 @@
+# shellcheck shell=bash
+
+mkdir ~/.ssh
+cp .github/workflows/known_hosts ~/.ssh/known_hosts
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git fetch origin refs/notes/nix-store:refs/notes/nix-store
+
+finish() {
+    ./x.py fetch-notes
+    git push origin refs/notes/nix-store
+}
+trap finish EXIT
+
+./x.py ci
