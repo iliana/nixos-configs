aec979c19703e9cf9f4e1c86b1f68f2563d892f2
iliana etaoin <iliana@buttslol.net>
2023-06-13 06:57:46 +0000

    hydrangea /var/www

--- hosts/hydrangea/default.nix
+++ hosts/hydrangea/default.nix
@@ -8,9 +8,19 @@
   ];
 
   iliana.caddy.virtualHosts = with config.iliana.caddy.helpers; {
+    "209.251.245.209:80" = serve "/var/www/209.251.245.209";
     "haha.business" = serve ./haha.business;
     "hydrangea.ili.fyi" = handle "/yo" "respond yo";
   };
+
+  iliana.persist.directories =
+    builtins.map (d: {
+      directory = "/var/www/${d}";
+      user = "iliana";
+      group = "users";
+    }) [
+      "209.251.245.209"
+    ];
 
   iliana.containerNameservers = ["8.8.8.8" "8.8.4.4" "2001:4860:4860::8888" "2001:4860:4860::8844"];
   networking.firewall.allowedTCPPorts = [80 443];
