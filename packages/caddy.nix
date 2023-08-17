d3d58519570594e61ba135b02be1177b12f0a3f7
iliana etaoin <iliana@buttslol.net>
2023-08-17 19:56:41 +0000

    update caddy

--- packages/caddy.nix
+++ packages/caddy.nix
@@ -3,7 +3,7 @@
   buildGoModule,
   fetchFromGitHub,
 }: let
-  version = "2.7.3";
+  version = "2.7.4";
 in
   buildGoModule {
     inherit (caddy) pname subPackages nativeBuildInputs postInstall passthru meta;
@@ -13,9 +13,9 @@ in
       owner = "caddyserver";
       repo = "caddy";
       rev = "v${version}";
-      hash = "sha256-KezKMpx3M7rdKXEWf5XUSXqY5SEimACkv3OB/4XccCE=";
+      hash = "sha256-oZSAY7vS8ersnj3vUtxj/qKlLvNvNL2RQHrNr4Cc60k=";
     };
-    vendorHash = "sha256-mTHEM+0yakKiy4ZFi+2qakjSlKFyRkbjeXOXdvx+9lA=";
+    vendorHash = "sha256-CnWAVGPrHIjWJgh4LwJvrjQJp/Pz92QHdANXZIcIhg8=";
 
     ldflags = caddy.ldflags ++ ["-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"];
   }
