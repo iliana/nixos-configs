6ae727356de95b947deda6cfbb74d82eb9478b85
iliana etaoin <iliana@buttslol.net>
2023-09-05 04:14:44 +0000

    work around to bring bridge up after fastd restart

--- hosts/juice/phone-vlan.nix
+++ hosts/juice/phone-vlan.nix
@@ -11,10 +11,21 @@
   systemd.services.fastd-phone = {
     after = ["network.target"];
     wantedBy = ["multi-user.target"];
+
+    # ensure ordering is correct in activation script below
+    before = ["systemd-udev-trigger.service"];
+
     serviceConfig = {
       ExecStart = "${pkgs.fastd}/bin/fastd -c %d/config";
       ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
       LoadCredentialEncrypted = "config:${./fastd.conf.enc}";
     };
   };
+
+  # workaround for (roughly) https://github.com/NixOS/nixpkgs/issues/195777
+  system.activationScripts.restart-udev.text = ''
+    if ! ${pkgs.diffutils}/bin/cmp {/run/current-system,"$systemConfig"}/etc/systemd/system/fastd-phone.service >/dev/null; then
+      echo "systemd-udev-trigger.service" >>/run/nixos/activation-restart-list
+    fi
+  '';
 }
