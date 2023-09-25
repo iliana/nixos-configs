eface344a549346fbb171d43a4c8c1c1b81b43d1
iliana etaoin <iliana@buttslol.net>
2023-09-25 05:44:04 +0000

    will builds work?

--- modules/buildHost.nix
+++ modules/buildHost.nix
@@ -1,7 +1,12 @@
 {config, ...}: {
-  users.users.build.group = "build";
-  users.users.build.isSystemUser = true;
-  users.users.build.useDefaultShell = true;
+  users.users.build = {
+    group = "build";
+    isSystemUser = true;
+    useDefaultShell = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZKG2ahudgw86zX+ZBdYqMC2rp3691M7Dz8aE4+1Wiy"
+    ];
+  };
   users.groups.build = {};
 
   nix.settings.trusted-users = ["root" "build"];
