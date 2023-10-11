71df754ab639b27aa0c8c6f2de059801db59ba7c
iliana etaoin <iliana@buttslol.net>
2023-10-11 06:30:58 +0000

    deal with transmission darwin fix

--- packages/transmission/default.nix
+++ packages/transmission/default.nix
@@ -1,9 +1,13 @@
 {
   nixpkgs-unstable,
   callPackage,
+  darwin,
 }: let
   libutp = callPackage (nixpkgs-unstable + "/pkgs/applications/networking/p2p/libutp/3.4.nix") {};
-  transmission = callPackage (nixpkgs-unstable + "/pkgs/applications/networking/p2p/transmission/4.nix") {inherit libutp;};
+  transmission = callPackage (nixpkgs-unstable + "/pkgs/applications/networking/p2p/transmission/4.nix") {
+    inherit libutp;
+    inherit (darwin.apple_sdk_11_0.frameworks) Foundation;
+  };
 in
   transmission.overrideAttrs (old: {
     patches = [
