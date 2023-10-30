5cba2a630b46fb64bfe472f01de1ffd054f60dc8
iliana etaoin <iliana@buttslol.net>
2023-10-30 00:53:08 +0000

    indicate this cinny build is a fork

---
+++ packages/cinny/README.md
@@ -0,0 +1,10 @@
+This is a set of patches applied on top of a version of [Cinny](https://github.com/cinnyapp/cinny).
+
+To get a source tree:
+
+```
+git clone -b v3.1.0 https://github.com/cinnyapp/cinny.git
+git clone https://github.com/iliana/nixos-configs.git
+cd cinny
+git am ../nixos-configs/packages/cinny/*.patch
+```
--- packages/cinny/default.nix
+++ packages/cinny/default.nix
@@ -1,13 +1,19 @@
 {
   nixpkgs-unstable,
   callPackage,
+  lib,
 }: let
   cinny = callPackage (nixpkgs-unstable + "/pkgs/applications/networking/instant-messengers/cinny") {};
 in
   cinny.overrideAttrs (old: {
+    version =
+      if (lib.hasInfix "-b v${old.version}" (builtins.readFile ./README.md))
+      then "${old.version}-iliana"
+      else builtins.throw "update the version in packages/cinny/README.md";
     patches =
       (old.patches or [])
       ++ [
-        ./0001-remove-mod-number-shortcuts.patch
+        ./0001-indicate-this-is-a-fork.patch
+        ./0002-remove-mod-number-shortcuts.patch
       ];
   })
